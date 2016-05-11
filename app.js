5f35ab77f90395cb629ddbbaa86ae5821563a330
Elev8 <staavd@yahoo.com>
2016-05-11 16:00:18 +0200

    11-05-2016

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-angular.module('app', []).controller("MainController", function() {
-    this.num1 = 0;
-    this.num2 = 0;
-  });
+          angular.module('myApp', []).controller('namesCtrl', function($scope) 
+            {
+              $scope.names = 
+              [
+                {fname:'Иван',ename:'Николайчук',email:'ivan@example.com'},
+                {fname:'Мария',ename:'Иванова',email:'mary@example.com'},
+                {fname:'Юлия',ename:'Петренко',email:'julia@example.com'}
+              ];
+              $scope.orderByMe=function(x)
+              {
+                $scope.myOrderBy=x;
+              }
+            }
+        );
