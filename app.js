ea937fd49a979ba38f48498ad6851e93b151e8cc
Elev8 <staavd@yahoo.com>
2016-05-16 15:33:09 +0200

    16-05-2016

--- app.js
+++ app.js
@@ -5,15 +5,13 @@ function person()
   this.mail;
 };
 
-var per = new person;
-
-         
 
           angular.module('myApp', []).controller
           ('namesCtrl', 
             function($scope) 
             {
-              $scope.names = [];
+             /* $scope.names = [];*/
+             $scope.names=[]; 
              /* [
                 {fname:'Иван',ename:'Николайчук',email:'ivan@example.com'},
                 {fname:'Мария',ename:'Иванова',email:'mary@example.com'},
@@ -23,17 +21,19 @@ var per = new person;
               {
                 $scope.myOrderBy=x;
               }
-
+                /*var per = new person;*/
                 $scope.addItem = function ()
-                 {
+                 {  per = new person;
+
                     per.fname=$scope.fname;
                     per.ename=$scope.ename;
                     per.mail=$scope.mymail;  
                     $scope.names.push(per);
-
+                    /*per=null;*/
                     console.log($scope,' person=',per);
                     console.log('$scope.fname,$scope.ename,$scope.mymail=',$scope.fname,$scope.ename,$scope.mymail );
-                    console.log ('names= ', $scope.names[1]);
+                    /*console.log ('names= ', names);*/
+                    console.log ('$scope= ', $scope);
                  } 
             }
           );
